265581e13669cad2c8555de7b5d8fa3403742f6f
jeremy-fong <112743562+jeremy-fong@users.noreply.github.com>
2022-10-13 01:56:46 -0700

    added javascript to check answer and display next question

--- assets/script.js
+++ assets/script.js
@@ -7,28 +7,132 @@ var answerBtnEl = document.getElementById('ansBtn')
 var resultsEl = document.getElementById('results');
 
 var quizEndEl = document.getElementById('quiz-end');
-var finalScore = document.getElementById('final-score');
+var finalScoreEl = document.getElementById('final-score');
 var scoreFormEl = document.getElementById('score-form');
 var initialsEl = document.getElementById('initials');
 
-var hofLink = document.getElementById('hof');
+var linkHof = document.getElementById('hof');
 var hallOfFame = document.getElementById('hall-of-fame');
 var scoresList = document.getElementById('scores-list');
 var clearBtn = document.getElementById('clear');
 var backBtn = document.getElementById('go-back'); 
 
-
+var scores = [];
 let questionIndex = 0;
 var secondsLeft = 60;
-
-
+let timerInterval;
+let flashTimeout;
 
 function startQuiz(){
     hideElement(introEl);
     showElement(questionContainerEl);
     availableQuestions = questionBank;
     displayQuestion();
+    startTimer();
+    // resetQuestions();
 }
+
+function displayQuestion() {
+    var currentQuestion = availableQuestions[questionIndex];
+    questionEl.textContent = currentQuestion.question;
+    var possibleAnswers = currentQuestion.possChoices;
+    for (i=0; i < possibleAnswers.length; i++) {
+        var button = document.createElement('button');
+        button.textContent = possibleAnswers[i];
+        button.setAttribute('class', 'btn')
+        button.addEventListener('click', function(event){
+            checkAnswer(event);
+            resetQuestions();
+            nextQuestion();
+        });
+        
+        answerBtnEl.appendChild(button);
+        
+    }
+};
+
+function resetQuestions() {
+    questionIndex++;
+    if (questionIndex < availableQuestions.length) {
+        while (answerBtnEl.firstChild) {
+            answerBtnEl.removeChild(answerBtnEl.firstChild);
+        }
+        // nextQuestion();  
+    } else {
+        endQuiz();
+
+    }
+}
+
+
+function checkAnswer(event) {
+    var selectedEl = event.target;
+    var selected = selectedEl.textContent;
+    var msgEl = document.createElement('h4');   
+    
+    if (selected === questionBank[questionIndex].correctAnswer) {
+        resultsEl.setAttribute('class', 'right');
+        msgEl.innerText = "Right!";
+        resultsEl.appendChild(msgEl);
+        flashTimout = setTimeout(function() {
+            hideElement(msgEl);
+            clearTimeout(flashTimout);
+            
+        },800); 
+    } else {
+        secondsLeft -= 10;
+        resultsEl.setAttribute('class', 'wrong');
+        msgEl.innerText = 'Wrong!';
+        resultsEl.appendChild(msgEl);
+        flashTimout = setTimeout(function() {
+            hideElement(msgEl);
+            clearTimeout(flashTimout);
+        },800);
+    }
+    
+    nextQuestion();
+    
+}
+
+function nextQuestion() {
+    if (questionIndex < questionBank.length) {
+        displayQuestion();
+    } else {
+        setTimeout(function() {
+            endQuiz();
+        },500)
+    }
+}
+
+
+var questionBank = [
+    {
+        question: "Commonly used datatypes do NOT include:",
+        possChoices: ['1. string', '2. boolean', '3. alerts', '4. numbers'],
+        correctAnswer: "3. alerts"
+    },
+    {
+        question: "The condition in an if/else statement is enclosed with ____.",
+        possChoices: ['1. quotes', '2. curly braces', '3. parenthesis', '4. square brackets'],
+        correctAnswer: "3. parenthesis"
+    },
+    {
+        question: "Arrays in javascript can be used to store ____.",
+        possChoices: ['1. numbers and strings', '2. booleans', '3. other arrays', '4. all of the above'],
+        correctAnswer: "4. all of the above"
+    },
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        possChoices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
+        correctAnswer: "4. console.log"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+        possChoices: ["1. <script href='xxx.js'>", "2. <script name='xxx.js'>", "3. <script value='xxx.js'>", "4. <script src='xxx.js'>"],
+        correctAnswer: "4. <script src='xxx.js'>"
+    },
+    
+];
 
 function showElement(element) {
     element.removeAttribute('class', 'hide');
@@ -38,55 +142,8 @@ function hideElement(element) {
     element.setAttribute('class', 'hide');
 }
 
-
-function displayQuestion() {
-    var currentQuestion = availableQuestions[questionIndex];
-    questionEl.innerText = currentQuestion.question;
-    var possibleAnswers = currentQuestion.possChoices;
-    for (i=0; i < possibleAnswers.length; i++) {
-        var button = document.createElement('button');
-        button.innerText = possibleAnswers[i];
-        button.setAttribute('class', 'btn')
-        button.addEventListener('click', function(){
-            
-        });
-        
-        answerBtnEl.appendChild(button);
-        
-    }
-}
-
-var questionBank = [
-    {
-        question: "Commonly used datatypes do NOT include:",
-        possChoices: ['1. string', '2. boolean', '3. alerts', '4. numbers'],
-        answer: "3. alerts"
-    },
-    {
-        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-        possChoices: ['1. JavaScript', '2. terminal/bash', '3. for loops', '4. console.log'],
-        answer: "4. console.log"
-    },
-    {
-        question: "The condition in an if/else statement is enclosed with ____.",
-        possChoices: ['1. quotes', '2. curly braces', '3. parenthesis', '4. square brackets'],
-        answer: "3. parenthesis"
-    },
-    {
-        question: "Arrays in javascript can be used to store ____.",
-        possChoices: ['1. numbers and strings', '2. booleans', '3. other arrays', '4. all of the above'],
-        answer: "4. all of the above"
-    },
-    {
-        question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
-        possChoices: ["a. <script href='xxx.js'>", "b. <script name='xxx.js'>", "c. <script value='xxx.js'>", "d. <script src='xxx.js'>"],
-        answer: 3
-    },
-
-];
-
 function startTimer() {
-    var timerInterval = setInterval(function() {
+     timerInterval = setInterval(function() {
         timerEl.textContent = secondsLeft;
         secondsLeft--;
         if (secondsLeft === 0) {
@@ -96,5 +153,5 @@ function startTimer() {
     },1000);
 }
 
-startBtn.onclick = startTimer;
+// startBtn.onclick = startTimer;
 startBtn.addEventListener("click", startQuiz);
